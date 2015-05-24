9e830d4e97f6dc4ad0492b927d7c84ae1625fa8f
adam j hartz <hartz@mit.edu>
2015-05-24 12:11:04 -0400

    oops

--- xonsh/ast.py
+++ xonsh/ast.py
@@ -142,7 +142,6 @@ class CtxAwareTransformer(NodeTransformer):
         inscope = self.is_in_scope(body)
         if not inscope:
             node.body = self.try_subproc_toks(body)
-            node.body[0] = self.visit(node.body[0])
         return node
 
     def visit_Expr(self, node):
